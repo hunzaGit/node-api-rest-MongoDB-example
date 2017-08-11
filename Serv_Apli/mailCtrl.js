69ec1abedfe2cdda3733f12059e4091007521f96
hunzaGit <rodrigo.trazas@gmail.com>
2017-08-11 16:12:05 +0200

    mejorado un poco el estilo de HTML del correo

--- Serv_Apli/mailCtrl.js
+++ Serv_Apli/mailCtrl.js
@@ -10,11 +10,11 @@ exports.sendEmail = function (req, res, callback = Function()) {
     var fromEmail = new helper.Email('rodrigo.trazas@gmail.com');
     var toEmail = new helper.Email('rodrigo.trazas@gmail.com');
     var subject = 'Registro adoptaUnAnimal prueba';
-    var content = new helper.Content('text/HTML', "<html><body>Holaa Rodri,<br>" +
-        "Esto es una prueba de email desde <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Logo_di_Heroku.png'  width='150' height='50' alt=''HerokuLogo> " +
-        "con texto escrito en <b>HTML</b> usando el Add-On <img src='https://avatars3.githubusercontent.com/u/181234?v=4&s=200' width='100' height='100'>, " +
-         "y esta es la foto de mi careto <br>"+
-            "<img align='center' src='http://s2.quickmeme.com/img/f2/f22c50f29387e1461274eb73ae3a329e97e3aa09ac8dffee9218e017cd6c8b99.jpg' width='360' height='200'>");
+    var content = new helper.Content('text/HTML', "<html><body>Holaa Rodri,"+
+    "<br> Esto es una prueba de email desde <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Logo_di_Heroku.png' width='150' height='50' " +
+        "style='margin-bottom: -16px'> con texto escrito en <b>HTML</b> usando el Add-On <img src='https://avatars3.githubusercontent.com/u/181234?v=4&s=200' width='100' height='100' style='margin-bottom: -39px'>," +
+    " y esta es la foto de mi careto"+
+    "<br><br><img src='http://s2.quickmeme.com/img/f2/f22c50f29387e1461274eb73ae3a329e97e3aa09ac8dffee9218e017cd6c8b99.jpg' width='360' height='200' ></body></html>");
     var mail = new helper.Mail(fromEmail, subject, toEmail, content);
 
     var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
